f398a6a9af5b655f9bdec62dfe0360a60b8d806f
rnathaniel45 <nathanielrandii@gmail.com>
2025-05-30 23:36:22 -0500

    watch

--- src/main/fileHandler.ts
+++ src/main/fileHandler.ts
@@ -1,1 +1,41 @@
-import
+import { dialog } from "electron";
+import * as fs from "node:fs/promises";
+
+async function addFile(): Promise<string> {
+    const filePath: string = (
+        await dialog.showOpenDialog({
+            title: "Choose a folder",
+            properties: ["openDirectory"]
+        })
+    ).filePaths[0];
+
+    return filePath;
+}
+
+interface watchResponse {
+    readonly success: boolean,
+    readonly abort?: AbortController
+};
+
+type watchCallback = (fileName: string) => void;
+
+async function watchFolder(filePath: string, callback: watchCallback): Promise<watchResponse> {
+    const abort: AbortController = new AbortController();
+
+    try {
+        const watcher = fs.watch(filePath, { signal: abort.signal });
+
+        (async () => {
+            for await (const event of watcher)
+                if (event.eventType === "rename" && event.filename)
+                    callback(event.filename);
+        })().catch(error => console.log(`Aborting watch (${filePath}): ${(error as Error).message}`)); //CHANGE TO IPC
+    } catch (error) {
+        console.log(`Failed to init watch (${filePath}): ${(error as Error).message}`);
+        return { success: false };
+    }
+
+    return { success: true, abort };
+}
+
+export { addFile, watchFolder };
